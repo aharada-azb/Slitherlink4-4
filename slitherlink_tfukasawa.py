f5c69b054a2b7f84d7f3a2b79876b6f671ec09ce
tfukasawa <f.20th.git@gmail.com>
2016-02-06 10:50:23 +0900

    Update slitherlink_tfukasawa.py

--- slitherlink_tfukasawa.py
+++ slitherlink_tfukasawa.py
@@ -5,7 +5,7 @@ col=[[1 for i in range(N)] for j in range(M+1)]
 row=[[1 for i in range(N+1)] for j in range(M)]
 
 def link(s,t):
-    if s<4 and t<4:
+    if s<N and t<M:
         c=col[s][t]+col[s][t-1]+row[s][t]+row[s-1][t]
     elif s==N and t!=M:
         c=col[s][t]+col[s][t-1]+row[s-1][t]
